c88b6f3c5f033bcfc75cc6fc07f0e502a9d45d4d
Daniel Kelly <me@danielkelly.io>
2021-11-12 12:21:05 -0600

    boilerplate auth user composable

--- src/composables/UseAuthUser.js
+++ src/composables/UseAuthUser.js
@@ -1,7 +1,17 @@
 import { ref } from "vue";
 const user = ref(null);
 export default function useAuthUser() {
+  const login = ({ email, password }) => {};
+  const logout = () => {};
+  const register = ({ username, email, password, meta }) => {};
+  const update = (data) => {};
+  const sendPasswordRestEmail = () => {};
   return {
     user,
+    login,
+    logout,
+    register,
+    update,
+    sendPasswordRestEmail,
   };
 }
