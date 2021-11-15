2523fd6ee936dd6f3ef243f8cc0b9bc9aca50187
Daniel Kelly <me@danielkelly.io>
2021-11-15 16:03:45 -0600

    profile page

---
+++ src/pages/Me.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useRouter } from "vue-router";
+import useAuthUser from "@/composables/UseAuthUser";
+
+// use necessary composables
+const router = useRouter();
+const { user, isLoggedIn } = useAuthUser();
+
+// redirect to login if not logged in
+if (!isLoggedIn()) router.replace({ name: "Login" });
+</script>
+
+<template>
+  <div v-if="user">
+    <div>Hello {{ user.user_metadata.name }}</div>
+  </div>
+</template>
