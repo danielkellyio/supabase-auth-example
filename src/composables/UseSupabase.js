8223dbc009ce8728ea4c1991843ab26bc9b3c7cc
Daniel Kelly <me@danielkelly.io>
2021-11-15 15:59:51 -0600

    onAuthStateChange

--- src/composables/UseSupabase.js
+++ src/composables/UseSupabase.js
@@ -1,5 +1,6 @@
 // UseSupabase.js
 import { createClient } from "@supabase/supabase-js";
+import useAuthUser from "@/composables/UseAuthUser";
 
 // these can come from an environment variable if desired
 // not required however as they are 100% exposed on the client side anyway
@@ -10,6 +11,17 @@ const supabaseKey =
 
 // setup client
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+// setup auth state listener
+supabase.auth.onAuthStateChange((event, session) => {
+  // the "event" is a string indicating what trigger the state change (ie. SIGN_IN, SIGN_OUT, etc)
+  // the session contains info about the current session most importanly the user dat
+  const { user } = useAuthUser();
+
+  // if the user exists in the session we're logged in
+  // and we can set our user reactive ref
+  user.value = session?.user || null;
+});
 
 // expose supabase client
 export default function useSupabase() {
