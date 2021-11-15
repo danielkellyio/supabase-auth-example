4cfd63ce1b1343312908d6ba05ba936b492cba98
Daniel Kelly <me@danielkelly.io>
2021-11-15 15:44:32 -0600

    use supabase sdk to fill in AuthUser composable

--- src/composables/UseAuthUser.js
+++ src/composables/UseAuthUser.js
@@ -1,3 +1,4 @@
+import useSupabase from "@/composables/UseSupabase";
 import { ref } from "vue";
 
 // user is set outside of the useAuthUser function
@@ -5,47 +6,85 @@ import { ref } from "vue";
 const user = ref(null);
 
 export default function useAuthUser() {
+  const { supabase } = useSupabase();
+
   /**
    * Login with email and password
    */
-  const login = async ({ email, password }) => {};
+  const login = async ({ email, password }) => {
+    const { user, error } = await supabase.auth.signIn({ email, password });
+    if (error) throw error;
+    return user;
+  };
 
   /**
    * Login with refresh token
    * Useful for logging in after email confirmations
    */
-  const loginWithRefreshToken = async (token) => {};
+  const loginWithRefreshToken = async (token) => {
+    const { user, error } = await supabase.auth.signIn({ refreshToken: token });
+    if (error) throw error;
+    return user;
+  };
 
   /**
    * Login with google, github, etc
    */
-  const loginWithSocialProvider = (provider) => {};
+  const loginWithSocialProvider = async (token) => {
+    const { user, error } = await supabase.auth.signIn({ provider });
+    if (error) throw error;
+    return user;
+  };
 
   /**
    * Logout
    */
-  const logout = async () => {};
+  const logout = async () => {
+    const { error } = supabase.auth.signOut();
+    if (error) throw error;
+  };
 
   /**
    * Check if the user is logged in or not
    */
-  const isLoggedIn = () => {};
+  const isLoggedIn = () => {
+    return !!user.value;
+  };
 
   /**
    * Register
    */
-  const register = async ({ email, password, ...meta }) => {};
+  const register = async ({ email, password, ...meta }) => {
+    const { user, error } = await supabase.auth.signUp(
+      { email, password },
+      //arbitrary meta data is passed as the second argument under a data key
+      // to the Supabase signUp method
+      { data: meta }
+    );
+    if (error) throw error;
+    return user;
+  };
 
   /**
    * Update user email, password, or meta data
    */
-  const update = async (data) => {};
+  const update = async (data) => {
+    const { user, error } = await supabase.auth.update(data);
+    if (error) throw error;
+    return user;
+  };
 
   /**
    * Send user an email to reset their password
    * (ie. support "Forgot Password?")
    */
-  const sendPasswordRestEmail = async (email) => {};
+  const sendPasswordRestEmail = async (email) => {
+    const { user, error } = await supabase.auth.api.resetPasswordForEmail(
+      email
+    );
+    if (error) throw error;
+    return user;
+  };
 
   /**
    * Will be useful for informing the application what to do
